c127387e28a39e800bf491ddbfe93162000d6927
John Guberman <jeguberman@gmail.com>
2018-01-02 12:13:09 -0500

    session controller

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,26 @@
+class Api::SessionsController < ApplicationController
+  #copied from benchbnb
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      login(@user)
+      render "api/users/show"
+    else
+      render json: ["Bad username or password"], status: 401 #401 authorization required
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      logout
+      render "api/users/show"
+    else
+      render json: ["Must be loggined to logout"], status: 404 #404 not find
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :show, :update]
-    resource :session, only: [:create, :destroy, :show]
+    resource :session, only: [:create, :destroy] #why does benchbnb have a show route for sessions? their controller does not
     resources :images
     resources :comments
     # resources :image_likes, only: [:create, :destroy]
