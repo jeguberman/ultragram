08549728551b9372676ac6589094f24b5abcea09
John Guberman <jeguberman@gmail.com>
2018-01-02 12:05:42 -0500

    more routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
-    resources :user, only: [:create, :show, :update]
+    resources :users, only: [:create, :show, :update]
     resource :session, only: [:create, :destroy, :show]
+    resources :images
+    resources :comments
+    # resources :image_likes, only: [:create, :destroy]
+    # resources :comment_likes, only: [:create, :destroy]
   end
 
   root "static_pages#root"
