02d9c0e679dff802be460ff5cfd0cfd1cb80713f
John Guberman <jeguberman@gmail.com>
2018-01-04 16:19:05 -0500

    adjust users table and update seeds, create seed for demo user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180102164150) do
+ActiveRecord::Schema.define(version: 20180104204121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,9 @@ ActiveRecord::Schema.define(version: 20180102164150) do
     t.string "session_token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_picture_url"
+    t.string "personal_statement"
+    t.string "fullname", null: false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 
 User.delete_all
 
-User.create(username:"Cthulu", password:"passwordryleh")
-User.create(username:"thanos", password:"deathandjewels")
-User.create(username:"vvDoom", password:"drrichardssucks")
+User.create(username:"demo", password:"password", fullname:"demo user", personal_statement:"I am a fictitious hypothetical entitity who's existence is temporally bound between login and logout")
+User.create(username:"Cthulu", fullname:"error", password:"passwordryleh", personal_statement:"ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn")
+User.create(username:"thanos", password:"deathandjewels", fullname:"dione")
+User.create(username:"vvDoom", password:"reedrichardssucks", fullname: "Victor Van Damme")
