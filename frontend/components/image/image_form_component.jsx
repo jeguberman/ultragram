7802a651a19d86a7f4c680245cfd60d6cc68d564
John Guberman <jeguberman@gmail.com>
2018-01-10 16:21:49 -0500

    further style changes to form

--- frontend/components/image/image_form_component.jsx
+++ frontend/components/image/image_form_component.jsx
@@ -10,69 +10,68 @@ class ImageForm extends React.Component{
 
   }
 
-  handleSubmit(e){
 
-    e.preventDefault();
-    const formData = new FormData();
-    formData.append("image[image_url]", this.state.imageFile);
-    formData.append("image[caption]", this.state.caption);
-    this.props.postImage(formData);
-    if(!this.props.errors){
-      this.props.history.push("/");
-    }
-  }
+  imageField(){
+    return(
+      <div className='image-form-image'>
 
-
-
-  handleFileChange(e){
-    const reader = new FileReader();
-    const file = e.currentTarget.files[0];
-
-    reader.onloadend = () => {
-      return( this.setState( {image_url: reader.result, imageFile: file} ) );
-    };
-
-    if(file){
-      reader.readAsDataURL(file);
-    }else{
-      this.setState({image_url:"", imageFile: null});
-    }
-
-  }
-
-  update(field){
-    return (e) => {
-      this.setState({[field]:e.target.value});
-    };
+      </div>
+    );
   }
 
   formComponent(){
     return (
-      <form className="image-form form" onSubmit={this.handleSubmit}>
+      <form className="image-form form grey-border" onSubmit={this.handleSubmit}>
 
-        <input type="file" onChange={this.handleFileChange}/>
 
-        <img src={this.state.image_url} />
+        <div className="image-form-buffer"></div>
+        <img className='image-form-image' src={this.state.image_url} />
+
 
         <input type="text" value={this.state.caption} className="image-form-caption" placeholder="Write a caption for your image..." onChange={this.update('caption')}/>
+
         {this.listErrors()}
-        <input type="submit" className="image-form-submit" value="Submit Image" />
+
+        <div className="image-form-buttons">
+
+        <label className="file-upload-mask session-button">
+          Choose file...
+          <input type="file" onChange={this.handleFileChange}
+            id="file-upload"/>
+        </label>
+
+
+          <input type="submit" className="image-form-submit session-button" value="Submit Image" />
+
+        </div>
       </form>
     );
   }
 
-  isEdit(){
-    if(this.props.formType==="editImage"){
-      return(<div>is edit</div>);
-    }
+
+
+  render (){
+
+    return(
+      <div>
+        {this.formComponent()}
+        {this.isEdit()}
+      </div>
+    );
   }
 
-  listErrors(){
-    if(this.props.errors){
-      const errorList = this.props.errors.map(
-        (error, idx) => {
-          return <li key={idx}
-            className="image-form-error-item error">{error}</li>;
+isEdit(){
+  if(this.props.formType==="editImage"){
+    return(<div>is edit</div>);
+  }
+}
+
+listErrors(){
+  if(this.props.errors){
+    const errorList = this.props.errors.map(
+      (error, idx) => {
+        return <li key={idx}
+          className="image-form-error-item error">{error}</li>;
         }
       );
 
@@ -85,16 +84,40 @@ class ImageForm extends React.Component{
     }
   }
 
+handleSubmit(e){
 
-  render (){
-
-    return(
-      <div>
-        {this.formComponent()}
-        {this.isEdit()}
-      </div>
-    );
+  e.preventDefault();
+  const formData = new FormData();
+  formData.append("image[image_url]", this.state.imageFile);
+  formData.append("image[caption]", this.state.caption);
+  this.props.postImage(formData);
+  if(!this.props.errors){
+    this.props.history.push("/");
   }
 }
 
+handleFileChange(e){
+  const reader = new FileReader();
+  const file = e.currentTarget.files[0];
+
+  reader.onloadend = () => {
+    return( this.setState(
+      {image_url: reader.result, imageFile: file}
+    ));
+  };
+
+  if(file){
+    reader.readAsDataURL(file);
+  }else{
+    this.setState({image_url:"", imageFile: null});
+  }
+
+}
+
+update(field){
+  return (e) => {
+    this.setState({[field]:e.target.value});
+  };
+}
+}
 export default ImageForm;
--- frontend/components/image/image_show_component.jsx
+++ frontend/components/image/image_show_component.jsx
@@ -9,41 +9,66 @@ class ImageShow extends React.Component{
     this.props.fetchImage(this.props.id);
   }
 
-  renderImage(){
-    return(
-        <img src={this.props.image.image_url}
-          className="image image-show-image" />
-    );
+  render(){
+    if(this.props.author){
+      return(
+        <div className="image-show grey-border">
+          {this.renderImage()}
+          {this.socialBlock()}
+        </div>
+      );
+    }else{
+      return(null);
+    }
   }
 
+
+  // socialBlock(){
+  //   return(
+  //     <div className="image-show-social-block">
+  //       {this.authorInfo()}
+  //       {this.comments()}
+  //
+  //       {this.badges()}
+  //       {this.likes()}
+  //       {this.elapsedTime()}
+  //       <input type="text" className="comment-input" placeholder="Add a comment..."/>
+  //     </div>
+  //   );
+  // }
+
+  socialBlock(){
+    return(
+      <div className="image-show-social-block">
+        {this.authorInfo()}
+        {this.comments()}
+
+
+        {this.elapsedTime()}
+        <div className="comment-input"></div>
+      </div>
+    );
+  }
 
   authorInfo(){
-    let author = this.props.author;
+  let author = this.props.author;
+  return(
+    <header className="image-show-author-info author-info-container comment-item">
+      <img className="user-picture image-show-author-picture" src={author.profile_image_url} />
+      <div className="author-name">{author.username}</div>
+              {this.userFollowed()}
+    </header>
+  );
+    }
+
+  comments(){
     return(
-      <header className="author-info-container comment-item">
-        <img className="user-picture image-show-author-picture" src={author.profile_image_url} />
-        <div className="author-name">{author.username}</div>
-                {this.userFollowed()}
-      </header>
-    );
-  }
 
-  userFollowed(){
-    return(null);
-  }
+      <div className="comment-block">
+        {this.imageCaption()}
 
-
-  elapsedTime(){
-    return(
-      <Moment className="elapsed-time feed-item-comment" fromNow>{this.props.image.created_at}</Moment>
-    );
-  }
-
-  likes(){
-    return(
-      <div className="image-show-likes like-block">
-        <div className="image-show-view-count view-count">like block</div>
       </div>
+
     );
   }
 
@@ -62,73 +87,39 @@ class ImageShow extends React.Component{
     }
   }
 
+  renderImage(){
+    return(
+        <img src={this.props.image.image_url}
+          className="image image-show-image" />
+    );
+  }
+
   badges(){
     return(
       <div className="image-show-badges">badges
        </div>
      );
-  }
+   }
 
-
-
-  socialBlock(){
+  likes(){
     return(
-      <div className="image-show-social-block">
-        {this.authorInfo()}
-        {this.comments()}
-
-        {this.badges()}
-        {this.likes()}
-        {this.elapsedTime()}
-        <input type="text" className="comment-input" placeholder="Add a comment..."/>
+      <div className="image-show-likes like-block">
+        <div className="image-show-view-count view-count">like block</div>
       </div>
     );
   }
 
-  comments(){
+  elapsedTime(){
     return(
-
-      <div className="comment-block">
-        {this.imageCaption()}
-        <div className="comment">
-          <div className="author-name">
-            {this.props.author.username}
-          </div>
-
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec semper neque, id lacinia est. Nunc fringilla arcu massa. Nullam quis lacus lacinia, malesuada risus sed, scelerisque leo. Donec nec tellus vitae eros vestibulum semper. Curabitur sodales imperdiet nulla, in aliquet eros volutpat ut. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur dictum volutpat sapien et varius. Sed nec placerat eros. Mauris dapibus molestie enim vitae suscipit. Nunc eleifend pellentesque justo sit amet iaculis. Suspendisse nec arcu varius, convallis elit in, fermentum quam.
-
-        </div>
-
-        <div className="comment">
-          <div className="author-name">
-            {this.props.author.username}
-          </div>
-
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec semper neque, id lacinia est. Nunc fringilla arcu massa. Nullam quis lacus lacinia, malesuada risus sed, scelerisque leo. Donec nec tellus vitae eros vestibulum semper. Curabitur sodales imperdiet nulla, in aliquet eros volutpat ut. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur dictum volutpat sapien et varius. Sed nec placerat eros. Mauris dapibus molestie enim vitae suscipit. Nunc eleifend pellentesque justo sit amet iaculis. Suspendisse nec arcu varius, convallis elit in, fermentum quam.
-
-        </div>
-
-      </div>
-
+      <Moment className="elapsed-time feed-item-comment" fromNow>{this.props.image.created_at}</Moment>
     );
   }
 
-
-
-
-
-  render(){
-    if(this.props.author){
-      return(
-        <div className="image-show grey-border">
-          {this.renderImage()}
-          {this.socialBlock()}
-        </div>
-      );
-    }else{
-      return(null);
-    }
+  userFollowed(){
+    return(null);
   }
+
 }
+
 
 export default ImageShow;
