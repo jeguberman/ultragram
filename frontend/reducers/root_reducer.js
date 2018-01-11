bfdeec4972829515e74dbab15fce72524326d299
John Guberman <jeguberman@gmail.com>
2018-01-11 08:52:12 -0500

    refactored root reducer. added seeds for sunwukong

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -3,15 +3,13 @@ import session from './session_reducer';
 import errors from './errors_reducer';
 import images from './images_reducer';
 import users from './users_reducer';
-import sessErrs from './session_errors_reducer';
 
 const RootReducer = combineReducers(
   {
     session,
     users,
     images,
-    errors,
-    sessErrs
+    errors
   }
 );
 
