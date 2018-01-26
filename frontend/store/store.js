8809efbc2a955851f42e0431fffdef4ec63a41d4
John Guberman <jeguberman@gmail.com>
2018-01-26 17:17:46 -0500

    hopefully remove minification warning

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,13 +1,19 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 import RootReducer from '../reducers/root_reducer';
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const {logger } = require('redux-logger');
+  middlewares.push(logger);
+}
 
 const configureStore = (preLoadedState = {}) => {
   return (
     createStore(RootReducer,
       preLoadedState,
-      applyMiddleware(thunk, logger))
+      applyMiddleware(...middlewares))
   )
 }
 
